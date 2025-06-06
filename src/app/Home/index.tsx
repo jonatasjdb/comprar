a6a2dca15d21472bdfa0e4367db4cef3b81ef86c
Jônatas Dias <mac@MacBook-Pro-de-mac.local>
2025-06-06 18:10:17 -0300

    Finalizando o visual

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,20 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import { styles } from './styles'
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
+
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
+import { Item } from "@/components/Item";
+
 import { FilterStatus } from "@/types/FilterStatus";
 
+import { styles } from './styles'
+
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+const ITEMS = [
+  {id: "1", status: FilterStatus.DONE, description: "1 package coffe"},
+  {id: "2", status: FilterStatus.DONE, description: "3 package coffe"},
+  {id: "3", status: FilterStatus.PENDING, description: "6 package coffe"},
+]
 
 export function Home() {
   return (
@@ -22,11 +31,23 @@ export function Home() {
         {FILTER_STATUS.map((status) => (
             <Filter key={status} status={status} isActive />
         ))}
-
-        <TouchableOpacity style={styles.clearButton}>
-          <Text style={styles.clearText}>Limpar</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
         </View>
+        <FlatList
+          data={ITEMS}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <Item data={{status: item.status, description: String(item.description)}}
+              onStatus={() => console.log('Status')}
+              onRemove={() => console.log('Remove')}
+            />
+          )}
+          ItemSeparatorComponent={() =><View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
+        />
       </View>
     </View>
   )
